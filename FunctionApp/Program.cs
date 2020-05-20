ff0a7490d8e5fd84a88efd2aa7f1c85311add29f
Emre Bodur <30430161+eemrebodur@users.noreply.github.com>
2020-05-20 09:25:23 +0300

    Factorial Console App

---
+++ FunctionApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi, FakToplam = 1;
+            Console.Write("Faktöriyeli hesaplanacak sayı değerini giriniz = ");
+            sayi = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i < sayi; i++)
+                FakToplam += FakToplam * i;
+            Console.WriteLine(sayi + " sayısının faktoriyel sonucu = " + FakToplam);
+            Console.ReadLine();
+
+        }
+    }
+}
